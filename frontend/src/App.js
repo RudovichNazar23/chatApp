a91392335bc1aff4fb4eeac845915c064cf9b77c
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-07-16 20:08:03 +0200

    added registration form and modified login form with form container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./components/Home";
-import LoginForm from "./components/LoginForm";
-import RegistrationForm from "./components/RegistrationForm";
+import FormContainer from "./components/FormContainer";
 import PageNotFound from "./components/PageNotFound";
 import ProtectedRouter from "./components/ProtectedRouter";
 
@@ -16,8 +15,8 @@ function App() {
               <Home />
             </ProtectedRouter>
           } path="/"/>
-          <Route element={<LoginForm />} path="login"/>
-          <Route element={<RegistrationForm />} path="registration"/>
+          <Route element={<FormContainer />} path="login"/>
+          <Route element={<FormContainer />} path="registration"/>
           <Route element={<PageNotFound />} path="*"/>
         </Routes>
       </BrowserRouter>
--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -2,8 +2,6 @@ import "../index.css";
 
 import { useState } from "react";
 
-import FormContainer from "./FormComponents/FormContainer";
-import FormNavBar from "./FormComponents/FormNavBar";
 import FormHeader from "./FormComponents/FormHeader";
 import FormGroup from "./FormComponents/FormGroup";
 import FormErrorList from "./FormComponents/FormErrorList";
@@ -13,18 +11,13 @@ export default function LoginForm(){
     const [formErrors, setFormErrors] = useState([]);
 
     return (
-        <FormContainer>
-            <FormNavBar/>
-            <div className="mt-3">
-                <form className="p-3">
-                    <FormHeader headerValue={"Login"}/>
-                    <hr/>
-                    <FormGroup labelName={"Username"} inputType={"text"}/>
-                    <FormGroup labelName={"Password"} inputType={"password"}/>
-                    <FormErrorList formErrors={formErrors}/>
-                    <FormButton/>
-                </form>
-            </div>
-        </FormContainer>
+        <form className="p-3">
+            <FormHeader headerValue={"Login"}/>
+            <hr/>
+            <FormGroup labelName={"Username"} inputType={"text"}/>
+            <FormGroup labelName={"Password"} inputType={"password"}/>
+            <FormErrorList formErrors={formErrors}/>
+            <FormButton/>
+        </form>
     );
 };
