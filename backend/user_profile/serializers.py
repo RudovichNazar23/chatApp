3431fea41023d89eda1694ab3ae6d774a252e1af
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-07-17 20:53:20 +0200

    added submit handler for registration form

--- backend/user_profile/serializers.py
+++ backend/user_profile/serializers.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from rest_framework import serializers
+
 from rest_framework.serializers import ModelSerializer
 from django.contrib.auth.models import User
 
@@ -5,4 +9,17 @@ from django.contrib.auth.models import User
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "email", "first_name", "last_name"]
+        fields = ["id", "username", "password", "email", "first_name", "last_name"]
+    
+    def create(self, validated_data):
+        user = User(**validated_data)
+        password = validated_data["password"]
+
+        try:
+            validate_password(password=password)
+            user.set_password(password)
+            user.save()
+        except ValidationError as error:
+            raise serializers.ValidationError({"password": error.messages})
+        return user
+    
--- frontend/src/components/RegistrationForm.jsx
+++ frontend/src/components/RegistrationForm.jsx
@@ -1,17 +1,48 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import api  from "../utils/api";
+
 import FormHeader from "./FormComponents/FormHeader";
 import FormGroup from "./FormComponents/FormGroup";
 import FormErrorList from "./FormComponents/FormErrorList";
 import FormButton from "./FormComponents/FormButton";
 
 export default function RegistrationForm(){
+    const [formErrors, setFormErrors] = useState([]);
+    const navigate = useNavigate();
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+        const currentTarget = event.currentTarget;
+        const username = currentTarget.username.value;
+        const password = currentTarget.password.value;
+        const repeatPassword = currentTarget.repeatPassword.value;
+
+        if(!username || !password || !repeatPassword) return;
+
+        if(password !== repeatPassword) setFormErrors((prev) => prev.push("Passwords don't match"));
+
+        const response = api.post("user_profile/user/", { username: username, password: password });
+        response
+        .then((res) => {
+            if(res.status === 201) navigate("/");
+        })
+        .catch((error) => {
+           for(const key in error.response.data){
+            setFormErrors(error.response.data[key]);
+           }
+        });
+    };
     return (
-        <form className="p-3">
+        <form className="p-3" onSubmit={onSubmitHandler}>
             <FormHeader headerValue={"Registration"}/>
             <hr/>
-            <FormGroup labelName={"Username"}/>
-            <FormGroup labelName={"Password"} inputType={"password"}/>
-            <FormGroup labelName={"Repeat password"} inputType={"password"}/>
-            <FormErrorList formErrors={[]}/>
+            <FormGroup labelName={"Username"} inputId={"username"}/>
+            <FormGroup labelName={"Password"} inputType={"password"} inputId={"password"}/>
+            <FormGroup labelName={"Repeat password"} inputType={"password"} inputId={"repeatPassword"}/>
+            <FormErrorList formErrors={formErrors}/>
             <FormButton/>
         </form>
     );
