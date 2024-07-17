6961a8392f10f3bbb93835dbe69ce8a870de6f72
RudovichNazar23 <n.rudovich2005@gmail.com>
2024-07-17 21:21:34 +0200

    added permissions for user viewset

--- backend/user_profile/views.py
+++ backend/user_profile/views.py
@@ -4,8 +4,19 @@ import rest_framework.permissions as permissions
 from django.contrib.auth.models import User
 
 from .serializers import UserSerializer
+from .permissions import IsOwner
 
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
-    permission_classes = [permissions.AllowAny]
     queryset = User.objects.all()
+
+    def get_permissions(self):
+        permission_classes = []
+
+        if self.action == "create":
+            permission_classes = [permissions.AllowAny]
+        elif self.action in ("list",):
+            permission_classes = [permissions.IsAuthenticated]
+        else:
+            permission_classes = [permissions.IsAuthenticated, IsOwner]
+        return [permission() for permission in permission_classes]
